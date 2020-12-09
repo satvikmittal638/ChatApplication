687945ebcf0d2bb4a20a81ee0a8bc34bd04c369b
Satvik Mittal <satvikmittal638@gmail.com>
2020-12-09 09:36:12 +0530

    D

--- app/src/main/java/com/example/chatapplication/ActiveUsersFragment.java
+++ app/src/main/java/com/example/chatapplication/ActiveUsersFragment.java
@@ -3,15 +3,21 @@ package com.example.chatapplication;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.SearchView;
 
 import com.example.chatapplication.Model.userModel;
 import com.example.chatapplication.RecyclerViewActiveUsers.ActiveUsersAdapter;
